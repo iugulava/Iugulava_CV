977b914aca4e961a38b41f9017f79a4105e6f74a
Irakli Ugulava <i.ugulava@gmail.com>
2022-01-21 18:05:24 +0400

    navbar

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
-function logPerson(s, name, age) {
-    if (age < 0) {
-        age = "Еще не родился";
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {
+    myFunction();
+};
+
+// Get the header
+var header = document.getElementById("myHeader");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+    if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+    } else {
+        header.classList.remove("sticky");
     }
-    return `${s[0]}${name}${s[1]}${age}${s[2]}`;
 }
-
-const personName = "Владилен";
-const personName2 = "Максим";
-const personAge = 26;
-const personAge2 = -10;
-
-const output = logPerson `Имя: ${personName}, Возраст: ${personAge}!`;
-const output2 = logPerson `Имя: ${personName2}, Возраст: ${personAge2}!`;
-
-console.log(output);
-console.log(output2);
